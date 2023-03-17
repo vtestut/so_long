a26c809e1503c70a1f9833c41cc5184cd12e3cad
Virgile TESTUT <vtestut@e1r11p15.clusters.42paris.fr>
2023-03-17 15:56:24 +0100

    125%

--- bonus/libft/srcs/memory/ft_bzero.c
+++ bonus/libft/srcs/memory/ft_bzero.c
@@ -6,16 +6,11 @@
 /*   By: vtestut <vtestut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 19:21:25 by virgile           #+#    #+#             */
-/*   Updated: 2023/01/25 11:50:50 by vtestut          ###   ########.fr       */
+/*   Updated: 2023/03/17 15:10:48 by vtestut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// void	ft_bzero(void *s, size_t n)
-// {
-// 	ft_memset(s, 0, n);
-// }
 
 void	ft_bzero(void *s, size_t n)
 {
--- bonus/libft/srcs/strings/ft_strchr.c
+++ bonus/libft/srcs/strings/ft_strchr.c
@@ -6,25 +6,11 @@
 /*   By: vtestut <vtestut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 18:56:40 by virgile           #+#    #+#             */
-/*   Updated: 2023/02/14 15:53:57 by vtestut          ###   ########.fr       */
+/*   Updated: 2023/03/17 15:11:45 by vtestut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	char	t;
-
-// 	if (!s)
-// 		return (NULL);
-// 	t = (char) c;
-// 	while (*s != t && *s != '\0')
-// 			s++;
-// 	if (*s == t)
-// 		return ((char *)s);
-// 	return (NULL);
-// }
 
 char	*ft_strchr(const char *s, char c)
 {
